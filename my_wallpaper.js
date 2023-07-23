b907b1cb41749613fccfea12e8c597989ce9738f
Qing Qing He <qingqinghe916@hotmail.com>
2023-07-24 11:52:41 +1200

    added smile

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -2,6 +2,7 @@
 let rect_width  = 20;
 let rect_height = 20;
 
+let colorValue = 'yellow';
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(DEVELOP_GLYPH);
@@ -19,5 +20,36 @@ function wallpaper_background() {
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
+  let headX = 75;
+  let headY = 75;
+  let eyeY = headY - 10;
+  let earY = headY - 40;
+  let eyeSize = 10;
+  
+  fill(255);
+  stroke(0, 0, 0);
+
+
+
+  if (mouseIsPressed){
+    colorValue = 'red';
+  }
+  else {
+    colorValue = 'yellow';
+  }
+  fill(colorValue);
+  ellipse(headX, headY, 70, 70); //head
+
+  fill(0, 0, 0) // blue colour
+
+  ellipse(headX - 15, eyeY, eyeSize, eyeSize) //left eye // 60 before
+
+  ellipse(headX + 15, eyeY, eyeSize, eyeSize) // right eye
+  stroke(0, 0, 0);
+  arc(75, 90, 30, 15, 0, 180);
+
+  // line(headX - 5, headY + 10, headX + 5, headY + 5);
+  // line(headX + 5, headY + 10, headX - 5, headY + 5);
+
+
 }
