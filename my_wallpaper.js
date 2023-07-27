3ae58277e3fdba9a0e65498a34b2bd65c1059e71
Qing Qing He <qingqinghe916@hotmail.com>
2023-07-28 10:30:10 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -37,7 +37,36 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   ellipse(headX, headY, 70, 70); //head
 
   fill(255) // blue colour
-  ellipse(5, 5, 10, 10);
+  ellipse(45, 45, 20, 20);
+  ellipse(36, 58, 20, 20);
+  ellipse(32, 73, 20, 20);
+  ellipse(35, 85, 20, 20);
+  ellipse(42, 98, 20, 20);
+  ellipse(51, 108, 20, 20);
+  ellipse(63, 113, 20, 20);
+  ellipse(76, 113, 20, 20);
+  ellipse(90, 110, 20, 20);
+  ellipse(105, 103, 20, 20);
+  
+  ellipse(103, 47, 20, 20);
+  ellipse(111, 58, 20, 20);
+  ellipse(114, 73, 20, 20);
+  ellipse(113, 88, 20, 20);
+  strokeWeight(5);
+  point(88, 88);
+  point(68, 99);
+  point(80, 99);
+  point(60, 88);
+  strokeWeight(1);
+  noFill();
+  beginShape();
+  curveVertex(88, 88);
+  curveVertex(88, 88);
+  // curveVertex(68, 99);
+  // curveVertex(80, 99);
+  curveVertex(60, 88);
+  curveVertex(60, 88);
+  endShape();
 
    //ellipse(headX - 15, eyeY, eyeSize, eyeSize) //left eye 
 
