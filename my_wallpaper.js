39dd298ee25b094ba306bfa11c4d3e56738987af
Qing Qing He <qingqinghe916@hotmail.com>
2023-08-05 13:55:28 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -23,107 +23,98 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   let headX = 75;
   let headY = 75;
   let eyeY = headY - 10;
-  let earY = headY - 40;
+  // let earY = headY - 40;
  // let eyeSize = 10;
   let noiseSize = 5;
 
-  
-  draw_face(headX, headY, 20, '#f7db4f', '#ec2049');
-  
-  
-  
-  //stroke(0, 0, 0);
-  //line(80, 80, 100, 100);
- // fill('yellow');
-  // ellipse(headX, headY, 70, 70); //head
+  if (background_color !=='#363636')
+  {
+    console.log('true');
+  }
+  else {
+    console.log('false');
+  }
 
-  // fill(255) // blue colour
-  // ellipse(84, 35, 20, 20);
-  // ellipse(69, 34, 20, 20);
-  // ellipse(56, 38, 20, 20);
-  // ellipse(45, 45, 20, 20);
-  // ellipse(36, 58, 20, 20);
-  // ellipse(32, 73, 20, 20);
-  // ellipse(35, 85, 20, 20);
-  // ellipse(42, 98, 20, 20);
-  // ellipse(51, 108, 20, 20);
-  // ellipse(63, 113, 20, 20);
-  // ellipse(76, 113, 20, 20);
-  // ellipse(90, 110, 20, 20);
-  // ellipse(105, 103, 20, 20);
+  draw_colorfulface(headX, headY, 20, '#f7db4f', '#ec2049');
+  //draw_face(headX, headY, 20, '#f7db4f', '#ec2049');
   
-  
-  // ellipse(99, 43, 20, 20);
-  // ellipse(111, 58, 20, 20);
-  // ellipse(114, 73, 20, 20);
-  // ellipse(113, 88, 20, 20);
- 
-  // strokeWeight(1); //mouth
-  // noFill();
-  // beginShape();
-  // curveVertex(88, 88);
-  // curveVertex(88, 88);
-  // curveVertex(80, 95);
-  // curveVertex(68, 95);
-  // curveVertex(60, 88);
-  // curveVertex(60, 88);
-  // endShape();
   draw_eye(headX, eyeY, 10, 15);
   draw_mouth('#ff847c');
-  draw_bigeye('green', 'orange', 80);
+  draw_bigeye('green', 'orange', 10, 80);
 
-  // ellipse(headX - 15, eyeY, eyeSize, eyeSize) //left eye 
-
-  // ellipse(headX + 15, eyeY, eyeSize, eyeSize) // right eye
-
-  // noStroke(0, 0, 0);
-  // fill('green');
-  // ellipse(150, 43 + positionY, 55, 55);
-  // fill('orange');
-  // ellipse(150, 43 + positionY, 35, 35); //Big eye
-  // fill(255)
-  // ellipse(145, 35 + positionY, 16, 16);
-  // ellipse(154, 54 + positionY, 12, 12);
-  
 }
 
 function draw_face(headX, headY, faceSize, faceColor,sideColor)
 {
   stroke(0, 0, 0);
-  //line(80, 80, 100, 100);
   fill(faceColor);
   ellipse(headX, headY, 70, 70); //head
-
   fill(sideColor) // blue colour
-  ellipse(84, 35, faceSize, faceSize);
-  ellipse(69, 34, faceSize, faceSize);
-  ellipse(56, 38, faceSize, faceSize);
-  ellipse(45, 45, faceSize, faceSize);
-  ellipse(36, 58, faceSize, faceSize);
-  ellipse(32, 73, faceSize, faceSize);
-  ellipse(35, 85, faceSize, faceSize);
-  ellipse(42, 98, faceSize, faceSize);
-  ellipse(51, 108, faceSize, faceSize);
-  ellipse(63, 113, faceSize, faceSize);
-  ellipse(76, 113, faceSize, faceSize);
-  ellipse(90, 110, faceSize, faceSize);
-  ellipse(105, 103, faceSize, faceSize);
-  
-  
-  ellipse(99, 43, faceSize, faceSize);
-  ellipse(111, 58, faceSize, faceSize);
-  ellipse(114, 73, faceSize, faceSize);
-  ellipse(113, 88, faceSize, faceSize);
+  ellipse(84, 31, faceSize, faceSize);
+  ellipse(64, 32, faceSize, faceSize);
+  ellipse(47, 41, faceSize, faceSize);
+  ellipse(35, 56, faceSize, faceSize);
+  ellipse(31, 75, faceSize, faceSize);
+  ellipse(35, 94, faceSize, faceSize);
+  ellipse(47, 110, faceSize, faceSize);
+  ellipse(65, 118, faceSize, faceSize);
+  ellipse(85, 118, faceSize, faceSize);
+  ellipse(103, 109, faceSize, faceSize);
+  ellipse(103, 39, faceSize, faceSize);
+  ellipse(115, 56, faceSize, faceSize);
+  ellipse(119, 75, faceSize, faceSize);
+  ellipse(116, 94, faceSize, faceSize);
+}
+
+function draw_colorfulface(headX, headY, faceSize, faceColor,sideColor)
+{
+  stroke(0, 0, 0);
+  fill(faceColor);
+  ellipse(headX, headY, 70, 70); //head
+  // fill(sideColor) // blue colour
+  fill('#e1f5c4');
+  ellipse(84, 31, faceSize, faceSize);
+  fill('#ede574');
+  ellipse(64, 32, faceSize, faceSize);
+  fill('#f9d423');
+  ellipse(47, 41, faceSize, faceSize);
+  fill('#fc913a');
+  ellipse(35, 56, faceSize, faceSize);
+  fill('#ff4e50');
+  ellipse(31, 75, faceSize, faceSize);
+  fill('#e1f5c4');
+  ellipse(35, 94, faceSize, faceSize);
+  fill('#ede574');
+  ellipse(47, 110, faceSize, faceSize);
+  fill('#f9d423');
+  ellipse(65, 118, faceSize, faceSize);
+  fill('#fc913a');
+  ellipse(85, 118, faceSize, faceSize);
+  fill('#ff4e50');
+  ellipse(103, 109, faceSize, faceSize);
+  fill('#a7226e');
+  ellipse(103, 39, faceSize, faceSize);
+  fill('#ec2049');
+  ellipse(115, 56, faceSize, faceSize);
+  fill('#f26b38');
+  ellipse(119, 75, faceSize, faceSize);
+  fill('##f7db4f');
+  ellipse(116, 94, faceSize, faceSize);
 }
 
 function draw_eye(headX, eyeY, eyeSizeX, eyeSizeY)
 {
   stroke(0, 0, 0);
-  fill('white');
+  fill('#363636');
   strokeWeight(1);
   ellipse(headX - 15, eyeY, eyeSizeX, eyeSizeY) //left eye 
 
   ellipse(headX + 15, eyeY, eyeSizeX, eyeSizeY) // right eye
+  fill('white');
+  ellipse(60, 60, 7, 7) // left eye
+  ellipse(58, 67, 6, 6) // left eye
+  ellipse(90, 60, 7, 7) // right eye
+  ellipse(92, 67, 6, 6) // right eye
 
 }
 
@@ -142,13 +133,13 @@ function draw_mouth(mouthColor){
     line(88,88, 60,88);
 }
 
-function draw_bigeye(outsideColor, insideColor, positionY){
+function draw_bigeye(outsideColor, insideColor,positionX,  positionY){
   noStroke(0, 0, 0);
   fill(outsideColor);
-  ellipse(150, 43 + positionY, 55, 55);
+  ellipse(150+positionX, 43 + positionY, 55, 55);
   fill(insideColor);
-  ellipse(150, 43 + positionY, 35, 35); //Big eye
+  ellipse(150+positionX, 43 + positionY, 35, 35); //Big eye
   fill(255)
-  ellipse(145, 35 + positionY, 16, 16);
-  ellipse(154, 54 + positionY, 12, 12);
+  ellipse(145+positionX, 35 + positionY, 16, 16);
+  ellipse(154+positionX, 54 + positionY, 12, 12);
 }
